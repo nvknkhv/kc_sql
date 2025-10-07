4e8f5791a2c386964337bbca6e10c078b95596d5
nvknkhv <n.v.konokhova@gmail.com>
2025-10-07 13:28:50 +0300

    chore: продолжение

--- 10 Оконки.sql
+++ 10 Оконки.sql
@@ -233,5 +233,127 @@ FROM   (SELECT time::date as date,
 ORDER BY date, order_type
 
 
+-- 10.13
+-- средняя цену всех товаров. Колонку с этим значением назовите avg_price.
+-- с помощью оконной функции и оператора FILTER рассчитайте среднюю цену товаров без учёта самого дорогого
+
+with cte AS (
+    SELECT  product_id,
+            name,
+            price,
+            AVG(price) OVER() as avg_price
+    FROM products 
+)
+SELECT  product_id,
+        name,
+        price,
+        ROUND(avg_price, 2) as avg_price,
+        ROUND(AVG(price) FILTER (WHERE price != (SELECT MAX(price) FROM cte)) OVER (), 2) as avg_price_filtered
+FROM cte        
+ORDER BY price DESC, product_id      
+
+
+-- 10.14
+
+
+-- 10.15
+-- Из таблицы courier_actions отберите топ 10% курьеров по количеству доставленных за всё время заказов
+
+WITH    cte AS (
+            SELECT  courier_id,
+                    COUNT(order_id) as orders_count
+            FROM courier_actions
+            WHERE action = 'deliver_order'
+            GROUP BY courier_id),
+        courier_count as 
+            (SELECT count(distinct courier_id)
+            FROM   courier_actions)
+        
+SELECT * FROM (
+            SELECT  courier_id,
+                    orders_count,
+                    ROW_NUMBER() OVER(ORDER BY orders_count DESC, courier_id) as courier_rank   -- ранги по кол-ву заказов
+            FROM cte) as t1
+WHERE  courier_rank <= round((SELECT * 
+                              FROM   courier_count)*0.1)    --общее число курьеров
+
+
+-- 10.16
+-- из таблицы courier_actions всех курьеров, которые работают в нашей компании 10 и более дней
+-- сколько заказов они уже успели доставить за всё время работы
+-- число дней, которые отработал курьер, — это количество дней, прошедших с первого принятого заказа до времени последней записи в таблице courier_actions
+
+
+WITH max_time AS (-- верхняя отметка времени - макс time по всем действиям
+    SELECT MAX(time)
+    FROM courier_actions
+    )
+
+SELECT  courier_id,
+        MAX(days_employed) as days_employed,
+        MAX(delivered_orders) as delivered_orders
+FROM (
+    SELECT  courier_id,
+            DATE_PART('days', 
+                (SELECT * FROM max_time ) 
+                - MIN(time) FILTER(WHERE action = 'accept_order') OVER(PARTITION BY courier_id  ORDER BY time))::int  as days_employed,     -- минимум по времени из только ПРИНЯТЫХ 
+            COUNT(order_id) FILTER(WHERE action = 'deliver_order') OVER(PARTITION BY courier_id) as delivered_orders  -- только ДОСТАВЛЕННЫЕ
+    FROM courier_actions
+    -- в итоге будет таблица где для каждого курьера будут колонки с днями и count, число строк = число строк в исход таблице, то есть есть дубли
+    ) as t
+GROUP BY courier_id, days_employed
+HAVING days_employed >=10
+ORDER BY days_employed DESC, courier_id
+
+Вариант верного решения:
+
+SELECT courier_id,
+       days_employed,
+       delivered_orders
+FROM   (SELECT courier_id,
+               delivered_orders,
+               date_part('days', max(max_time) OVER() - min_time)::integer as days_employed
+        FROM   (SELECT courier_id,
+                       count(distinct order_id) filter (WHERE action = 'deliver_order') as delivered_orders,
+                       min(time) as min_time,
+                       max(time) as max_time
+                FROM   courier_actions
+                GROUP BY courier_id) t1) t2
+WHERE  days_employed >= 10
+ORDER BY days_employed desc, courier_id
+
+
+
+
+
+
+-- 10.17
+-- На основе информации в таблицах orders и products рассчитайте стоимость каждого заказа, 
+-- ежедневную выручку сервиса и долю стоимости каждого заказа в ежедневной выручке, выраженную в процентах
+with orders_prices_days AS (
+    SELECT  order_id,
+            SUM(price) as order_price,
+            DATE(MAX(creation_time)) as date
+    FROM (
+        SELECT  order_id, 
+                UNNEST(product_ids) AS product_id,
+                creation_time
+        FROM orders AS unnest_orders) AS t2
+    JOIN products
+    USING(product_id) -- имеем таблицу order_id, product_id, price
+    GROUP BY(order_id)
+)
+SELECT  order_id, 
+        creation_time, 
+        order_price,
+        SUM(order_price) OVER(PARTITION BY date) as daily_revenue,
+        ROUND(100 *order_price::decimal /  SUM(order_price) OVER(PARTITION BY date), 3) as percentage_of_daily_revenue
+FROM orders
+JOIN orders_prices_days
+USING(order_id)
+WHERE order_id NOT IN (SELECT order_id FROM user_actions WHERE action = 'cancel_order')
+ORDER BY date DESC, percentage_of_daily_revenue DESC, order_id
+
+
 
 
